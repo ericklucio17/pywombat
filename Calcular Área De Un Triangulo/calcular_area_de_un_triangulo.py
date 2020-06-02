7b458b87585983f9007d1d6956b950dc279030d3
Erick Lucio <erick.adrian.lu@gmail.com>
2020-06-02 15:40:16 -0500

    Ejercicio de Calcular Área De Un Triangulo

---
+++ Calcular Área De Un Triangulo/calcular_area_de_un_triangulo.py
@@ -0,0 +1,11 @@
+def calcular_area_de_triangulo(base, altura):
+    return (base * altura) / 2
+
+
+base = int(input("Ingrese la base:"))
+altura = int(input("Ingrese la altura:"))
+
+resultado = calcular_area_de_triangulo(base, altura)
+
+print(f"El área del triangulo es: {resultado}")
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# pywombat 
+
+Ejercicios realizados en la plataforma de Pywombat.
+
+[Link de la plataforma](https://pywombat.com/).
+
+
