3638faddd90fb6c9b43feb9c90c1b093917a52c7
Erick Lucio <erick.adrian.lu@gmail.com>
2020-06-19 20:34:47 -0500

    Calcular El Radio De Un Circulo

---
+++ Calcular El Radio De Un Circulo/calcular_el_radio_de_un_circulo.py
@@ -0,0 +1,8 @@
+import math
+
+radio = float(input("Ingrese el radio del circulo:"))
+
+area = math.pi * pow(radio, 2)
+
+print("El área del circulo es: {0:.2f}, el resultado dela operación.".format(area))
+
